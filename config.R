254dd621ae1e08538ab4a382330d1cfdf1c22122
xxmdy <395567182@qq.com>
2025-07-01 05:14:53 +0800

    Create config.R

---
+++ config.R
@@ -0,0 +1,5 @@
+# 配置项，需手动填写自己的Steam API、Steam ID、Notion API、Database ID
+API_key <- "你的32位Steam API，保留双引号"
+steam_id <- "你的17位Steam ID，保留双引号"
+notion_token <- "你的Notion Integration Token（API），保留双引号"
+database_id <- "你的32位Notion Database ID，保留双引号" 
