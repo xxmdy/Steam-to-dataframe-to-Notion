c332933ced4b3ba7309e75c495d9023fee49f188
xxmdy <395567182@qq.com>
2025-07-01 11:19:46 +0800

    Update 3. Steam-to-Notion.R

--- 3. Steam-to-Notion.R
+++ 3. Steam-to-Notion.R
@@ -1,10 +1,10 @@
 # ==== Steam API和Steam ID配置 ====
-API_key <- "你的32位Steam API"
-steam_id <- "你的17位Steam ID"
+API_key <- "你的32位Steam API，保留双引号"
+steam_id <- "你的17位Steam ID，保留双引号"
 
 # ==== Notion API和Database ID（如果只想手动导入，可不获取） ====
-notion_token <- "你的Notion API"
-database_id <- "你的32位Notion Database ID"
+notion_token <- "你的Notion API，保留双引号"
+database_id <- "你的32位Notion Database ID，保留双引号"
 
 # 安装需要的R包
 install.packages(c(
