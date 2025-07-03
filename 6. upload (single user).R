b3954d9c9044f35ec3df8532f9af5fb4aca834d0
xxmdy <395567182@qq.com>
2025-07-03 22:03:01 +0800

    Update and rename 5. safe_utils.R to 6. upload (single user).R

---
+++ 6. upload (single user).R
@@ -0,0 +1,20 @@
+# ==== 🧪 上传测试（单个用户） ====
+df_test <- rbind(head(final_df,20), #前20行
+                 final_df[380:400, ], #第380行-400行
+                 tail(final_df, 20)) #最后20行
+
+run_upload_loop(
+  df = df_test,
+  database_id = database_id,
+  token = notion_token,
+  update_fields = update_fields_global,
+  is_main_game = is_main_game_global,
+  skip_playtime_web = skip_playtime_web_global
+)
+
+# ==== 🫣正式上传（单个用户） ====
+upload_steam_data_to_notion(
+  df = final_df,
+  database_id = database_id,
+  token = notion_token
+)
