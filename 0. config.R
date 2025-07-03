3936ba03a6ae6c1fbbe0c8b0a854cf6c724d563e
xxmdy <395567182@qq.com>
2025-07-03 21:44:14 +0800

    Update 0. config.R

--- 0. config.R
+++ 0. config.R
@@ -3,9 +3,3 @@ API_key <- "你的32位Steam API，保留双引号"
 steam_id <- "你的17位Steam ID，保留双引号"
 notion_token <- "你的Notion Integration Token（API），保留双引号"
 database_id <- "你的32位Notion Database ID，保留双引号" 
-
-# 安装需要的R包
-install.packages(c(
-  "httr2", "httr", "jsonlite", "dplyr", "tibble", "lubridate",
-  "openxlsx", "xml2", "rvest", "cli", "glue"
-))
