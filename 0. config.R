fdec2846858c9e203bafff3a415ba161a144c42b
xxmdy <395567182@qq.com>
2025-07-03 21:44:34 +0800

    Update 0. config.R

--- 0. config.R
+++ 0. config.R
@@ -1,5 +1,8 @@
 # 配置项，需手动填写自己的Steam API、Steam ID、Notion API、Database ID
+# ==== Steam API和Steam ID配置（必须获取） ====
 API_key <- "你的32位Steam API，保留双引号"
 steam_id <- "你的17位Steam ID，保留双引号"
-notion_token <- "你的Notion Integration Token（API），保留双引号"
-database_id <- "你的32位Notion Database ID，保留双引号" 
+
+# ==== Notion API和Database ID（如果只想手动导入，可不获取） ====
+notion_token <- "你的Notion API，保留双引号"
+database_id <- "你的32位Notion Database ID，保留双引号"
